56d2585ad1efbeaddc865d710db668ee0fda6472
Héctor Tavera <hectortavera@MacBook-Pro.local>
2020-07-16 01:59:11 -0700

    Pending revision on commit

--- ieee123/ica_internal.py
+++ ieee123/ica_internal.py
@@ -1,9 +1,16 @@
+
 import pandas as pd
 import re
 
+
 #In master_dict, key = obj, val = obj_dict
-master_dict = {}
+master_dict = {}   # master_dict = {obj_0: val_0, obj_1: val_1, ....... obj_n: val_n}
+
+
+                               #[0 [key,value], 1 [key,value]        ,  ........., n] index location of dictionary
 global recorder
+
+
 
 def on_init(t):
     '''
@@ -24,30 +31,61 @@ def on_init(t):
 
     #Create a dict of classes to check on_commit. Key = class. Value = information on how to find library properties of class.
         #RATING: set the threshold as a % of the max rating. DEVIATION: set the threshold as a +-% from the nominal rating.
-    ica_class_dict = {'underground_line':{'configuration':1, 'rating.summer.continuous':'rating','rating.winter.continuous':'rating'},\
-                         'overhead_line':{'configuration':1, 'rating.summer.continuous':'rating', 'rating.winter.continuous':'rating'},\
-                         'transformer':{'configuration':1, 'power_rating':'rating', 'powerA_rating':'rating', 'powerB_rating':'rating', 'powerC_rating':'rating', 'primary_voltage':'deviation', 'secondary_voltage':'deviation'},\
-                         'regulator':{'configuration':1, 'raise_taps':'limit', 'lower_taps':'limit'},\
-                         'substation':{'configuration':0, 'nominal_voltage':'deviation'},\
-                         'triplex_meter':{'configuration':0, 'nominal_voltage':'deviation'},\
-                         'meter':{'configuration':0, 'nominal_voltage':'deviation'}}
+    ica_class_dict = {'underground_line':{'configuration':1,
+                                          'rating.summer.continuous':'rating',        # ica_class_dict[0][0] -- 1
+                                          'rating.winter.continuous':'rating'},       # ica_class_dict['underground_line']['configuration'] -- 1
+                         'overhead_line':{'configuration':1,
+                                          'rating.summer.continuous':'rating',
+                                          'rating.winter.continuous':'rating'},
+                         'transformer':{'configuration':1,
+                                        'power_rating':'rating',
+                                        'powerA_rating':'rating',
+                                        'powerB_rating':'rating',
+                                        'powerC_rating':'rating',
+                                        'primary_voltage':'deviation',
+                                        'secondary_voltage':'deviation'},
+                         'regulator':{'configuration':1,
+                                      'raise_taps':'limit',
+                                      'lower_taps':'limit'},
+                         'substation':{'configuration':0,
+                                       'nominal_voltage':'deviation'},
+                         'triplex_meter':{'configuration':0,
+                                          'nominal_voltage':'deviation'},
+                         'meter':{'configuration':0,
+                                  'nominal_voltage':'deviation'}}
     
-    object_list = gridlabd.get("objects")
-    
+    object_list = gridlabd.get("objects")   # results given after running ieee123.glm, from this we withdraw the objects or results and store them in the variable name
+                                            # Only works after initialization has started
+                                            #alert ORDER MUST BE DEFINED OR KNOWN
+
+                                            #object_list = [(0,'a','b'), 1, 2, 3....,n]
+
+
+
     #In obj_dict, key = property & val = threshold. One for each object.
-    obj_dict = {}
+    obj_dict = {}    # obj_dict = {property_0: threshold_0, property_1: threshold_1, ......, property_n: threshold_n}
     
-    for obj in object_list:
-        obj_dict.clear()
+    for obj in object_list:                                                   #object_list = [(0,'a','b'), 1, 2, 3....,n]                                             
+        obj_dict.clear()                                                      #                  ^
         #Get the class of the object
-        obj_class = gridlabd.get_object(obj).get('class')
+        obj_class = gridlabd.get_object(obj).get('class')                     # obj_class = could be a meter, node, transformer (defined in ieee123.glm)
+        print(obj,obj_class)                                                  # obj_class = classType('meter')
+                                                                              #How gridlabd retrieves the 'class' @ documentation
         
-        if obj_class in ica_class_dict:
-            class_dict = ica_class_dict.get(obj_class)
-            #TODO: Make sure obj names all either do or do not have ica_
+        if obj_class in ica_class_dict:                                       # because class is a meter (true) then the if statement is executed 
+            class_dict = ica_class_dict.get(obj_class)                        # stores class dictionary associated to ica_class_dict given obj_class equal to meter -- 
+            #TODO: Make sure obj names all either do or do not have ica_      # 
             #Make a list of properties to check for that class
-            prop_list = list(class_dict.keys())
-            del prop_list[0]
+            
+            if 'ica_' == obj_class[0:4]:       # or obj_class.value[0:4]
+                print('it has ica_ prefix')
+            else:
+                print('no ica_ prefix')          
+            
+            prop_list = list(class_dict.keys())                               # given 'meter' -- prop_list = ['configuration', nominal_voltage]
+            del prop_list[0]                                                        #      at loop                     ^
+                                                                                        # investigate del function...
+
 
             #Iterate through those properties, appending to obj_dict
             for prop in prop_list:
@@ -83,10 +121,15 @@ def on_init(t):
                 #If the user didn't input a % or a boolean, set the threshold to the user input
                 else:
                     obj_dict[prop] = thresh
-                    
-            master_dict[obj] = obj_dict.copy()
             
-    gridlabd.warning(str(master_dict))
+
+                  
+            master_dict[obj] = obj_dict.copy() # ---> master_dict = {obj: obj_dict.copy()} ---> 
+            print(master_dict)
+
+    info_test = open("master_dictionary.csv","w") # Dump to a file checking purposes 
+    info_test.write(str(master_dict))
+    gridlabd.warning(str(master_dict)) 
     recorder = open("results.csv","w")
     recorder.write("time, object, property, value")
         
@@ -100,43 +143,43 @@ For each key in master dictionary, get the value for each property and compare i
 If threshold is exceeded, record the object, property, and value, and exit.
 '''
 
-def on_commit(t):
-    '''
-    Note: This code is not complete! Needs to be restructured to reflect changes to on_init.
-    This script should get current values for each property of interest for each object, and
-    compare it to the thresholds created in on_init. 
+# def on_commit(t):
+#     '''
+#     Note: This code is not complete! Needs to be restructured to reflect changes to on_init.
+#     This script should get current values for each property of interest for each object, and
+#     compare it to the thresholds created in on_init. 
     
-    If threshold is exceeded, record the object, property, and value, and exit.
+#     If threshold is exceeded, record the object, property, and value, and exit.
     
-    '''
+#     '''
     
-#    for obj in master_dict:
-#        obj_dict = master_dict.get(obj)
-#        for prop in obj_dict:
-#            
-#            #Get the current value for the given property
-#            prop_check = prop.replace('_min','')
-#            prop_check = prop_check.replace('_max','')
-#            val = gridlabd.get_value(obj, prop_check)
-#
-#            #Convert the string to a float
-#            non_decimal = re.compile(r'[^\d.]+')
-#            val = float(non_decimal.sub('',val))
-#            
-##            gridlabd.warning(obj)
-##            gridlabd.warning(prop_check)
-##            gridlabd.warning(str(val))
-#            
-#
-#            if '_min' in prop and val < obj_dict.get(prop):
-#                pass
+#     for obj in master_dict:
+#         obj_dict = master_dict.get(obj)
+#         for prop in obj_dict:
+#         #Get the current value for the given property
+#             prop_check = prop.replace('_min','')
+#             prop_check = prop_check.replace('_max','')
+#             val = gridlabd.get_value(obj, prop_check)
+
+#             #Convert the string to a float
+#             non_decimal = re.compile(r'[^\d.]+')
+#             val = float(non_decimal.sub('',val))
+           
+# #            gridlabd.warning(obj)
+# #            gridlabd.warning(prop_check)
+# #            gridlabd.warning(str(val))
+           
+
+#             if '_min' in prop and val < obj_dict.get(prop):
+#                 pass
 #                    #Record the time, object, property, and value.
 #                #TODO: Check the keys in this dictionary - code below is placeholder
-##                obj_props = gridlabd.get_object(obj)
-##                recorder.write('%s,%s,%s\n' % (obj_props['time'],obj_props['name'],obj_props['property'],obj_props['value']))
-#            if '_min' not in prop and val > obj_dict.get(prop):
-#                pass
-#            
-#            
-    return True
-
+# #                obj_props = gridlabd.get_object(obj)
+# #                recorder.write('%s,%s,%s\n' % (obj_props['time'],obj_props['name'],obj_props['property'],obj_props['value']))
+#             if 'n_min' not in prop and val > obj_dict.get(prop):
+#                 pass
+           
+           
+#     return True
+# forcing on_init() function to run using python3 in terminal instead of gridlabd
+# on_init(time())
