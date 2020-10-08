193c850cf43d87b707bdbb447da3ceb6dbc45a82
Sara Borchers <saraborc@stanford.edu>
2020-10-08 12:42:48 -0700

    started on_commit

--- ieee123/ica_internal_init.py
+++ ieee123/ica_internal_init.py
@@ -11,9 +11,7 @@ import pandas as pd
 import re
 import gridlabd
 import pdb
-
-
-# viol_df = pd.DataFrame(columns = ['Object','Class','Init Prop','Min Thresh','Max Thresh','Commit Prop','Value','Violation?','Time'])
+import math
 
 
 def on_init(t):
@@ -49,7 +47,6 @@ def on_init(t):
     # In thresh_dict, key = class, val = dictionary w/ info to set thresh
     #   rating: set the threshold as a % of the max rating
     #   deviation: set the threshold as a +-% from the nominal rating.
-    #Todo: add commit props as list for init props
     thresh_dict = {'underground_line':{'configuration':1,
                                           'rating.summer.continuous':['rating','current_out_A','current_out_B','current_out_C','current_in_A','current_in_B','current_in_C'],
                                           'rating.winter.continuous':['rating','current_out_A','current_out_B','current_out_C','current_in_A','current_in_B','current_in_C']},
@@ -111,23 +108,22 @@ def on_init(t):
                 user_input = gridlabd.get_global(obj_class + '.' + init_prop)
                 thresh_min = None
         
+
                 # If the user input is a percentage, set the threshold to be a % or a +- range of its library value
                 if '%' in user_input:
                     if 'taps' in init_prop:
                         gridlabd.warning('%s, class %s should not have a percentage as a threshold.' % (obj,obj_class))
                         continue
-                    
+
                     user_input = float(user_input.strip('%'))/100  
+                    if user_input < 0:
+                        gridlabd.warning('User input for %s, class %s must be a non-negative number or percentage, blank, or X.' % (obj,obj_class))
+                        continue
+
                     if thresh_class_dict.get(init_prop)[0] == 'rating':
-                        if user_input < 0.8 or user_input > 1.2:
-                            gridlabd.warning('User input for %s, class %s must be between 80 and 120 percent.' % (obj,obj_class))
-                            continue
                         thresh_max = lib_val * user_input
                       
                     elif thresh_class_dict.get(init_prop)[0] == 'deviation':
-                        if user_input < 0.0 or user_input > 0.2:
-                            gridlabd.warning('User input for %s, class %s must be between 0 and 20 percent.' % (obj,obj_class))
-                            continue
                         thresh_max = lib_val * (1.0 + user_input)
                         thresh_min = lib_val * (1.0 - user_input)                        
                                         
@@ -135,14 +131,14 @@ def on_init(t):
                 # If the user input a number, set the threshold to that number.
                 elif user_input.isnumeric():
                     user_input = float(user_input)
+                    if user_input < 0:
+                        gridlabd.warning('User input for %s, class %s must be a non-negative number or percentage, blank, or X.' % (obj,obj_class))
+                        continue
+
                     if 'taps' in init_prop:
                         gridlabd.warning('%s, class %s should not have a number as a threshold.' % (obj,obj_class))
                         continue
-                    
-                    if user_input < (lib_val * 0.8) or user_input > (lib_val * 1.2):
-                        gridlabd.warning('User input for %s, class %s must be between %f and %f.' % (obj,obj_class, lib_val*0.8, lib_val*1.2))
-                        continue
-                    
+                                        
                     thresh_max = float(user_input)
                 
                 # If the user input is blank, set the threshold to the library value.
@@ -158,10 +154,10 @@ def on_init(t):
                 # Append one row to the viol_df for each commit property for each obj                
                 for idx, commit_prop in enumerate(commit_prop_list):
                     # print(pd.DataFrame({'Object':[str(obj)],'Class':str(obj_class),'Init Prop':init_prop,'Min Thresh':thresh_min,'Max Thresh':thresh_max,'Commit Prop':str(commit_prop)},index=[idx]))
-                    df_dict[str(obj)+'.'+init_prop+'.'+commit_prop] = pd.DataFrame({'Object':[str(obj)],'Class':str(obj_class),'Init Prop':init_prop,'Min Thresh':thresh_min,'Max Thresh':thresh_max,'Commit Prop':str(commit_prop)},index=[idx])                                                      
+                    df_dict[str(obj)+'.'+init_prop+'.'+commit_prop] = pd.DataFrame({'Object':[str(obj)],'Class':str(obj_class),'Init Prop':init_prop,'Min Thresh':thresh_min,'Max Thresh':thresh_max,'Commit Prop':str(commit_prop),'Violation Value':None, 'Violation Time':None},index=[idx])                                                      
     
     viol_df = pd.concat(list(df_dict.values()))
-    viol_df.to_csv('viol_df.csv', index=False)   
+    viol_df.to_csv('viol_df_init.csv', index=False)   
 
     return True
       
@@ -173,84 +169,31 @@ If threshold is exceeded, record the object, property, and value, and exit.
 '''
 
 def on_commit(t):
+    for index, row in viol_df.iterrows():
+        # Get the real-time value for the property
+        obj = row['Object']
+        commit_prop= row['Commit Prop']
+        str_value = gridlabd.get_value(obj, commit_prop)
+        if 'j' in str_value:
+        # if row['Type'] == 'complex':
+            c_val = complex(str_value[:-2])
+            value = math.sqrt(c_val.real**2 + c_val.imag**2)
+        else:
+            value = float(re.sub("[^0-9]","",gridlabd.get_value(obj, commit_prop)))
 
-    # 1. Threshold value -> master ica_dictionary (set by user)
-    # 2. Simulation result -> object in GridLAB-D-> dictionary
-    # 3.
-
-    '''
-    Note: This code is not complete! Needs to be restructured to reflect changes to on_init.
-    This script should get current values for each property of interest for each object, and
-    compare it to the thresholds created in on_init. 
-    
-    If threshold is exceeded, record the object, property, and value, and exit.
-
-
-
-    '''
-    '''
-    
-     for node in nodes:
-        set the current value to the maximum with no violation 
-        set the voltage value to the maximum with no violation
+        # Compare it against the min and max threshold
+        # print(value)
+        # print(row['Max Thresh'])
+        if value > row['Max Thresh']:
+            viol_df.at[index,'Violation Time'] = 'time'
+            viol_df.at[index,'Violation Value'] = value
             
-            Simplified: 
-                Load setting (Voltage and current)
-                at this node reduce this specific load
-                
-                gridlabd.set(inverter)['voltaje'] = 2000
-        
-        and runs the power flow simulation for the next node to be analyzed
-     
-        ICA a current and voltage value 
-    
-    '''
-#     print('Commit model start')
-#     for obj in master_dict:
-#         obj_dict = master_dict.get(obj)
-#         for prop in obj_dict:
-#         #Get the current value for the given property
-#             print(prop) #This is a strg
-#             # prop_check = prop.replace('_min','') Commented out new variable defined prop_check
-#             prop.replace('_min', '')
-#             #print(prop)
-#             #possible if statement missing CHECK
-#             #prop_check = prop_check.replace('_max','')  #Commented out associated with prop_check
-#             #val = gridlabd.get_value(obj, prop_check)  #Commented out associated with prop_check
-
-#             #Convert the string to a float
-#             #non_decimal = re.compile(r'[^\d.]+')    #Commented out to avoid fatal error in run
-#             #val = float(non_decimal.sub('',val))
-
-# #            gridlabd.warning(obj)
-# #            gridlabd.warning(prop_check)
-# #            gridlabd.warning(str(val))
-
-# #   Comented out fatal error in run
-# #             if '_min' in prop and val < obj_dict.get(prop):
-# #                 pass
-# #                    #Record the time, object, property, and value.
-# #                #TODO: Check the keys in this dictionary - code below is placeholder
-# # #                obj_props = gridlabd.get_object(obj)
-# # #                recorder.write('%s,%s,%s\n' % (obj_props['time'],obj_props['name'],obj_props['property'],obj_props['value']))
-# #             #if 'n_min' not in prop and val > obj_dict.get(prop):  #typo in the line
-# #             if '_min' not in prop and val > obj_dict.get(prop):
-# #                 pass
-
-#         # Run the model until al the power injection sequences are completed
-#         # Run the model until the previous ica value is the same as the last calculated
+            if not row['Min Thresh'] == None:
+                if value < row['Min Thresh']:
+                    viol_df.at[index,'Violation Time'] = 'time'
+                    viol_df.at[index,'Violation Value'] = value
+    viol_df.to_csv('viol_df_commit.csv', index=False)   
+  
     return True
 
-###DUMP###
-#    
-#            if 'ica_' == obj_class[0:4]:
-#                print('it has ica_ prefix')
-#            else:
-#                print('no ica_ prefix')          
-#           
-    # info_test = open("master_dictionary.csv","w") # Dump to a file checking purposes 
-    # info_test.write(str(master_dict))
-    # gridlabd.warning(str(master_dict)) 
-    # recorder = open("results.csv","w")
-    # recorder.write("time, object, property, value")
  
