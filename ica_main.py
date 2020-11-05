c77b74864a440f78791bea97715bd4a3f4b5498e
Sara Borchers <saraborc@stanford.edu>
2020-11-05 10:01:25 -0800

    restructure using python wrapper

---
+++ ica_main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  5 09:42:40 2020
+
+@author: saraborchers
+"""
+import gridlabd
+# gridlabd.command("input_config.csv")
+# gridlabd.command("ica_mod.glm")
+gridlabd.command("ieee123/ica_ieee123.glm")
+gridlabd.start("wait")
