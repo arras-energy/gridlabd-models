c805def626c73cf664b7737680de6d9ab3f909d9
aivanova5 <ivanova.alyona5@gmail.com>
2020-11-05 10:10:43 -0800

    Setting the working path in ica_main

--- ICA_models/ica_main.py
+++ ICA_models/ica_main.py
@@ -6,7 +6,9 @@ Created on Thu Nov  5 09:42:40 2020
 @author: saraborchers
 """
 import gridlabd
+import os
+os.chdir("../ieee123")
 # gridlabd.command("input_config.csv")
 # gridlabd.command("ica_mod.glm")
-gridlabd.command("ieee123/ica_ieee123.glm")
+gridlabd.command("../ieee123/ica_ieee123.glm")
 gridlabd.start("wait")
