f3206d4ea06bc03f5aa5f9dba368baa906e1b3f8
Sara Borchers <saraborc@stanford.edu>
2020-11-06 14:32:32 -0800

    testing generic packaging

--- ICA_models/ica_main.py
+++ ICA_models/ica_main.py
@@ -7,8 +7,27 @@ Created on Thu Nov  5 09:42:40 2020
 """
 import gridlabd
 import os
-os.chdir("../ieee123")
-# gridlabd.command("input_config.csv")
-# gridlabd.command("ica_mod.glm")
-gridlabd.command("../ieee123/ica_ieee123.glm")
+
+print('running ica_main.py')
+
+# Run glm that modifies the IEEE model
+gridlabd.command('../ICA_models/ica_mod.glm')
+
+# Run the IEEE model
+# os.chdir("../")
+print(os.getcwd())
+gridlabd.command("../ieee123/model/ieee123.glm")
+
+print('Ran both glm files')
 gridlabd.start("wait")
+
+'''
+Question: I shouldn't have to have ica_config.glm in ICA_models
+for this to run, right? It should be created by the input file.
+But it doesn't run without it... different error when it is 
+included. 
+'''
+
+# play around with removing os, might be causing error
+
+# os.chdir("../ieee123")
