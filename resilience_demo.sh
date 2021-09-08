3023e740e724811ff683a1046a5fa9d71752075e
David P. Chassin <dchassin@slac.stanford.edu>
2021-09-08 12:33:04 -0700

    Update resilience_demo.sh

--- resilience_demo.sh
+++ resilience_demo.sh
@@ -197,6 +197,6 @@ debug "Input files:"
 # perform the main run
 #. ${OPENFIDO_RUN} || error $E_INTERNAL "${OPENFIDO_RUN} failed"
 cp -R ${EXECPATH}/ieee123/* ${OPENFIDO_INPUT}
-gridlabd -W ${OPENFIDO_INPUT} model/ieee123.glm -o ${OPENFIDO_OUTPUT}/model.html -D html_save_options="--show --cluster"
+gridlabd -W ${OPENFIDO_INPUT} model/ieee123.glm -o output/model.html -D html_save_options="--show --cluster"
 ls -lR ${OPENFIDO_INPUT}
-cp ${OPENFIDO_INPUT}/output/pole_data.csv ${OPENFIDO_OUTPUT}
+cp ${OPENFIDO_INPUT}/output/{pole_data.csv,model.html} ${OPENFIDO_OUTPUT} || true
